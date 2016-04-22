26f6c55184ef3bbca3419e1682adee293839ad44
Jonathan Linowes <jonathan@parkerhill.com>
2016-04-22 01:06:15 -0400

    set up new project

--- app/src/main/java/com/cardbookvr/visualizevr/MainActivity.java
+++ app/src/main/java/com/cardbookvr/visualizevr/MainActivity.java
@@ -1,13 +1,43 @@
 package com.cardbookvr.visualizevr;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.cardbookvr.renderbox.IRenderBox;
+import com.cardbookvr.renderbox.RenderBox;
+import com.cardbookvr.renderbox.Transform;
+import com.cardbookvr.renderbox.components.Cube;
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+
+public class MainActivity extends CardboardActivity implements IRenderBox {
+    private static final String TAG = "MainActivity";
+    CardboardView cardboardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+        cardboardView.setRenderer(new RenderBox(this, this));
+        setCardboardView(cardboardView);
+    }
+
+    @Override
+    public void setup() {
+        new Transform()
+                .setLocalPosition(0,0,-7)
+                .setLocalRotation(45,60,0)
+                .addComponent(new Cube(true));
+    }
+
+    @Override
+    public void preDraw() {
+
+    }
+
+    @Override
+    public void postDraw() {
+
     }
 }
